6374fd08195f89e0c772ca42245a3537feeed9e9
Evgeny <ku7nec0v@gmail.com>
2020-02-20 01:16:07 +0300

    refactor: request handler do_POST method

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import logging
 import json
@@ -8,7 +9,9 @@ from converter import CurrencyConverter
 from response import (
     ConvertResponse,
     ValueErrorResponse,
-    JsonDecodeErrorResponse
+    NotFoundResponse,
+    InvalidKeyResponse,
+    MethodNotAllowedResponse
 )
 
 
@@ -26,18 +29,57 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 # server settings
-HOST = 'localhost'
-PORT = 8080
+HOST = os.getenv('HOST') or 'localhost'
+PORT = os.getenv('PORT') or 8000
 
 
 class CurrencyConverterRequestHandler(BaseHTTPRequestHandler):
 
-    def __init__(self, *args, **kwargs):
+    """
+    Class handle only `POST` request with parameter `amount` in request.
+    For example valid request to sevice if it running on localhost and port
+    8000 using `httpie` python app:
+        - http POST http://loaclhost:8000/convert/ amount=10
+    """
+
+    def __init__(self, *args, **kwargs) -> None:
+        """Create converter and assign it to instance attribute."""
         self.converter = CurrencyConverter()
         super().__init__(*args, **kwargs)
 
-    def do_POST(self):
+    @property
+    def default_log_text(self):
+        """Default log text to start log message."""
+        return '{version} - {path} - Request from client {address} '.format(
+            version=self.request_version,
+            path=self.path,
+            address=self.client_address
+        )
+
+    def not_allowed_response(self, method: str) -> None:
+        """Create `MethodNotAllowedResponse` and handle request."""
+        MethodNotAllowedResponse(self).handle()
+        logger.error(
+            self.default_log_text +
+            'with method {method} not allowed.'.format(method=method)
+        )
+
+    def do_GET(self) -> None:
+        self.not_allowed_response('GET')
+
+    def do_PUT(self) -> None:
+        self.not_allowed_response('PUT')
+
+    def do_HEAD(self) -> None:
+        self.not_allowed_response('HEAD')
+
+    def do_POST(self) -> None:
+        """
+        Process request to convert passed amount. Only working with
+        path `/convert/`.
+        """
         if self.path == '/convert/':
+
             content_length = int(self.headers['Content-Length'])
             body = self.rfile.read(content_length)
 
@@ -45,15 +87,51 @@ class CurrencyConverterRequestHandler(BaseHTTPRequestHandler):
                 post_data = json.loads(body)
                 amount = post_data.get('amount')
 
-                try:
-                    ConvertResponse(self).handle(float(amount))
-                except ValueError:
-                    ValueErrorResponse(self).handle(amount)
-
+                if amount:
+                    try:
+                        ConvertResponse(self).handle(float(amount))
+                        logger.info(
+                            self.default_log_text +
+                            'to convert {amount} USD'.format(
+                                address=self.client_address,
+                                amount=amount
+                            )
+                        )
+                    except ValueError:
+                        ValueErrorResponse(self).handle(amount)
+                        logger.info(
+                            self.default_log_text +
+                            'to convert unappropriate type: {type} '
+                            'value: {value}'.format(
+                                address=self.client_address,
+                                type=type(amount),
+                                value=amount
+                            )
+                        )
+                else:
+                    InvalidKeyResponse(self).handle()
+                    logger.info(
+                        self.default_log_text +
+                        'with invalid `POST` parameters: {post_data}'.format(
+                            address=self.client_address,
+                            type=type(amount),
+                            post_data=post_data
+                        )
+                    )
             except JSONDecodeError:
-                JsonDecodeErrorResponse(self).handle()
+                InvalidKeyResponse(self).handle()
+                logger.info(
+                    self.default_log_text +
+                    'without any parameters'.format(
+                        address=self.client_address,
+                    )
+                )
         else:
-            JsonDecodeErrorResponse(self).handle()
+            NotFoundResponse(self).handle(self.path)
+            logger.info(
+                self.default_log_text +
+                '{path} not found'.format(path=self.path)
+            )
 
 
 if __name__ == '__main__':

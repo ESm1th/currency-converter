e49ef8c76f3276512634783a9b9424bb31a4c82a
Evgeny <ku7nec0v@gmail.com>
2020-02-20 00:28:37 +0300

    tests: tests for request handler

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import json
 import socket
 import warnings
+import unittest
 from unittest import TestCase
 from threading import Thread
 from http import HTTPStatus
@@ -13,6 +14,13 @@ from server import CurrencyConverterRequestHandler
 class TestAPI(TestCase):
 
     def setUp(self):
+        """
+        Create test fixtures:
+            - test server in separate thread
+            - connection object to send requests with it
+            - valid payload for `POST` requests
+            - invalid payload for `POST` requests
+        """
         warnings.simplefilter('ignore', ResourceWarning)
 
         host = 'localhost'
@@ -36,18 +44,96 @@ class TestAPI(TestCase):
         return port
 
     def make_request(self, method='GET', path=None, payload=None):
+        """
+        Make request with connection with appropriate method, path and payload.
+        Parameters:
+            - method: default='GET' represent HTTP method
+            - path: default=None
+        """
         self.connection.request(method, path, body=payload)
         response = self.connection.getresponse()
         self.connection.close()
         return response
 
-    def test_send_valid_request(self):
-        data = json.dumps(self.valid_payload)
-        response = self.make_request(
-            method='POST', path='/convert/',
-            payload=data
+    def make_post_convert_request(self, payload: str) -> None:
+        """
+        Retrun response from `POST` request on `convert` route.
+        Parameters:
+            - `payload`: json string from python dict.
+        """
+        return self.make_request(
+            method='POST',
+            path='/convert/',
+            payload=payload
         )
+
+    def test_send_valid_post_convert_request(self):
+        data = json.dumps(self.valid_payload)
+        response = self.make_post_convert_request(payload=data)
         self.assertEqual(response.status, HTTPStatus.OK)
+
+    def test_converter_convert(self):
+        data = json.dumps({'amount': 1})
+        unit_response = self.make_post_convert_request(payload=data)
+        one_usd = json.loads(unit_response.read()).get('result')
+
+        response = self.make_post_convert_request(
+            payload=json.dumps(self.valid_payload)
+        )
+        response_data = json.loads(response.read())
+
+        self.assertEqual(response.status, HTTPStatus.OK)
+        self.assertEqual(
+            response_data.get('result'),
+            one_usd * self.valid_payload['amount']
+        )
+
+    def test_value_error_response(self):
+        error = (
+            'Value of <amount> key in POST request should be '
+            'a digit, but <{passed}> were passed.'
+        ).format(passed=self.invalid_value_payload['amount'])
+
+        data = json.dumps(self.invalid_value_payload)
+        response = self.make_post_convert_request(payload=data)
+        response_data = json.loads(response.read())
+
+        self.assertEqual(response.status, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(response_data['error'], error)
+
+    def test_json_decode_error_response(self):
+        error = 'POST request should be used with parameter <amount>.'
+        data = json.dumps({'value': 10})
+        response = self.make_post_convert_request(payload=data)
+        response_data = json.loads(response.read())
+
+        self.assertEqual(response.status, HTTPStatus.BAD_REQUEST)
+        self.assertEqual(response_data['error'], error)
+
+    def test_path_does_not_exist(self):
+        url_path = '/not-exist/'
+        response = self.make_request('POST', path=url_path)
+        error = 'Path {url_pice} does not exist.'.format(url_pice=url_path)
+        response_data = json.loads(response.read())
+
+        self.assertEqual(response.status, HTTPStatus.NOT_FOUND)
+        self.assertEqual(response_data['error'], error)
+
+    def test_get_request(self):
+        response = self.make_request()
+        self.assertEqual(response.status, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_put_request(self):
+        response = self.make_request('PUT')
+        self.assertEqual(response.status, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_head_request(self):
+        response = self.make_request('HEAD')
+        self.assertEqual(response.status, HTTPStatus.METHOD_NOT_ALLOWED)
 
     def tearDown(self):
         self.server.shutdown()
+
+
+if __name__ == '__main__':
+    unittest.main()
